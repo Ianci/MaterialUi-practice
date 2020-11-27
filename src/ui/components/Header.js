22b2ebb60360067653869d343095b9ed7ca1a22b
ianci <ianbergia.rc@gmail.com>
2020-11-27 17:42:39 -0300

    agregar toolbar mixin para darle height al appbar y que no tape contenido

--- src/ui/components/Header.js
+++ src/ui/components/Header.js
@@ -2,7 +2,17 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import { makeStyles } from '@material-ui/core/';
+import logo from '../../assets/logo.svg
+'
+const useStyles = makeStyles(theme => ({
+    
+    toolbarMargin: {
+        ...theme.mixins.toolbar
+    }
+  }));
 
+//Material Ui AppBar function
 function ElevationScroll(props) {
     const { children} = props;
    
@@ -18,11 +28,20 @@ function ElevationScroll(props) {
   }
 
 export const HeaderPage = () => {
+   
+
+    const classes = useStyles();
+
     return (
+        <>
         <ElevationScroll>
         <AppBar position="fixed">
-            <Toolbar>Arc Development</Toolbar>
+            <Toolbar>
+               
+                </Toolbar>
         </AppBar>
         </ElevationScroll>
+        <div className={classes.toolbarMargin} />
+        </>
     )
 }
